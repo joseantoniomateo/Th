89c99cd79b19665586bff2307f8fac73742711cd
joseantoniomateo <jamateo@hotmail.es>
2014-03-04 13:00:16 +0100

    date

--- compile.sh
+++ compile.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 rm thesis.dvi
 rm thesis.pdf
+rm thesis.ps
+rm thesis.aux
 latex thesis.tex
-dvipdfm thesis.dvi
+latex thesis.tex
+#dvipdfm thesis.dvi
+dvips thesis.dvi
+ps2pdf thesis.ps
 evince thesis.pdf
 clear
